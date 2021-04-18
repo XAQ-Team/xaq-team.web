d57b4660c06fedf0a6e5cbc81de63b941c6adf05
Charles_Grozny <tayparov04@mail.ru>
2021-04-19 01:55:55 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 if (confirm("Ты готов зайди на сайт?\nОбязательно прочти Вопрос-ответ.")) {
 
 } else {
-    document.getElementById('h1').innerHTML = "Тогда зачем пришел?";
-    document.getElementById('a-1').hidden = false;
+    window.location.href = 'error.html';
 }
-
-
-
-
-
-
-
